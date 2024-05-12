fe56993fc30517485af6ba1e87fc4a815c1917ff
shuma-matsuzawa <november23thshuma@icloud.com>
2024-05-12 23:24:08 +0900

    'new'

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    async rewrites(){
+        return[
+            {
+                source: '/index.html',
+                destination:'/Expllosion/index.html',
+            },
+        ]
+    },
+}
